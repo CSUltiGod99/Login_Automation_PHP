2c6280f933ab1973504ee657ed968143ea6c3dd4
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-06-07 01:34:55 +0100

    Update Friend_suggestions_in_source.php

--- Friend_suggestions_in_source.php
+++ Friend_suggestions_in_source.php
@@ -8,10 +8,18 @@ function FriendSuggestion()
     $radiobutton
     $
     $
-    $
-    if():
+
+// Friends that are recommended by the user using friends_of_friends script:
+    $recommend_friend_text3 = "Shelly Shea";
+    $recommend_friend_text4 = "Brad Frederick";
+    $recommend_friend_text5 = "Carly Hill";
+    $recommend_friend_text6 = "Ron (Ronnie) Arte";
+    if($recommend_friend_text4 && $recommend_friend_text6 == true):
     {
-       $
+       echo $recommend_friend_text3;   
     }
+
+}
+
 
 
