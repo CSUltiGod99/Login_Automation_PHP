6ec485395eaf69e8faa49f08be8c1905f19b2807
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-07-11 03:11:33 +0100

    Update and rename Login_Page to Login_Page.php

---
+++ Login_Page.php
@@ -0,0 +1,12 @@
+// Login Page (Child):
+  
+fn WelcomeStatement()
+{
+      $eventAction = true;
+       $welcome_Dreamland = "Welcome to DreamLand";
+
+       if(eventAction == true)
+       {
+           echo welcome_Dreamland;
+       }
+}
